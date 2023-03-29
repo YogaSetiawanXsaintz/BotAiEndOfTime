0df66ae1dd8f4a3efd631a817c61a64bf36fea35
yogasetiawan <93601536+YogaSetiawanXsaintz@users.noreply.github.com>
2023-03-29 14:03:35 +0700

    Update help.js

--- command/help.js
+++ command/help.js
@@ -15,7 +15,7 @@ function toCommas(x) {
 exports.allMenu = (role, ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isCreator, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
     return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
 
-Hitung Mundur Idul Fitri 🌙 
+Hitung Mundur Idul Fitri Yages Ya🌙 
 ${mundur}
 
 *STATISTICS*
